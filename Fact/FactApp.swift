83eb291b20d307c633ae7dcba8a965f6b8706170
Ryan Yogan <ryanyogan@gmail.com>
2023-06-13 11:14:20 -0500

    Initial Commit

---
+++ Fact/FactApp.swift
@@ -0,0 +1,17 @@
+//
+//  FactApp.swift
+//  Fact
+//
+//  Created by Ryan Yogan on 6/13/23.
+//
+
+import SwiftUI
+
+@main
+struct FactApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
